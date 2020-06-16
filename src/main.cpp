92a20a602b1ac77f90806bbced4641082a5bd261
David Konerding <dakoner@gmail.com>
2020-06-16 13:53:32 -0700

    Add ArduinoOTA support.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@
 
 #include <WiFi.h>
 #include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+
 DRV8835MotorShieldEsp32 motors;
 
-const char* ssid = "UPSTAIRS";
-const char* password = "Recurser";
+const char* ssid = "";
+const char* password = "";
 
 //PID
 
@@ -66,12 +68,42 @@ void setup()
   pid.SetSampleTime(10);
   pid.SetOutputLimits(-100, 100 );
 
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
 }
 
 long c = millis();
 
 void loop()
 {
+  ArduinoOTA.handle();
+
   mpu_9250_loop();
   
   input = pitch;
